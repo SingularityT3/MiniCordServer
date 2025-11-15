816eb9a8a7581039450452ba7e70f2e4a40ac1a0
SingularityT3 <44658109+SingularityT3@users.noreply.github.com>
2025-11-15 13:18:45 +0530

    Profile pictures

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 import prisma from "../prisma.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { ObjectId } from "mongodb";
@@ -39,5 +42,53 @@ userRouter.get("/:id", async (req, res) => {
     res.status(200).json(user);
   } else {
     res.status(404).send();
+  }
+});
+
+// Profiles
+const mediaDir = "media/profiles/";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, mediaDir);
+  },
+  filename: (req: express.Request, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const filename = `${req.user!.id}${ext}`;
+    cb(null, filename);
+  }
+});
+
+const uploadProfile = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 2 },
+  fileFilter: (req, file, cb) => {
+    // Only allow images
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only image files are allowed"));
+    }
+    cb(null, true);
+  }
+})
+
+userRouter.post("/:id/profile", verifyToken, uploadProfile.single("avatar"), async (req, res) => {
+  if (req.user!.id !== req.params.id) {
+    return res.status(403).send();
+  }
+  if (!req.file) {
+    return res.status(400).send("No file uploaded");
+  }
+  res.status(201).send();
+});
+
+userRouter.get("/:id/profile", async (req, res) => {
+  try {
+    const files = fs.readdirSync(mediaDir);
+    const file = files.find(f => f.startsWith(req.params.id));
+    if (!file) {
+      throw new Error("File not found");
+    }
+    res.sendFile(path.resolve(mediaDir, file));
+  } catch (err) {
+    return res.status(404).send("Profile image not found");
   }
 });
